82ed711fce48d110da7b8d5a501575886fc98080
Prab <lifearoundpss@gmail.com>
2019-09-15 12:45:40 +0100

    parallax working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-console.log('test')
+document.addEventListener('DOMContentLoaded', function () {
+  const elems = document.querySelectorAll('.parallax')
+  const instances = M.Parallax.init(elems)
+})
