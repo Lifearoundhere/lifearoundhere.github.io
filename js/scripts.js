833fb3cb3885e2b072a45b737f5767e47f62dae1
Prab <lifearoundpss@gmail.com>
2019-09-19 15:35:32 +0100

    Added offline service worker

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('../sw.js').then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope)
+    }, function (err) {
+      console.log('ServiceWorker registration failed: ', err)
+    })
+  })
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
   const elems = document.querySelectorAll('.parallax')
   const instances = M.Parallax.init(elems)
